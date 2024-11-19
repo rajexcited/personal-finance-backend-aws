5958100e66dd7a9d385f62b9c48c50065b459e45
Raj <rajexcited@users.noreply.github.com>
2024-11-18 21:15:05 -0600

    changed to cli synthesizer instead of default synthesizer

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -12,9 +12,7 @@ const stack1 = new HelloCdkStack(app, "HelloCdkStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
-  synthesizer: new cdk.DefaultStackSynthesizer({
-    qualifier: `${appId}${infraEnv}`,
-  }),
+  synthesizer: new cdk.CliCredentialsStackSynthesizer(),
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
