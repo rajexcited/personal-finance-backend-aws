1f8939f2ed9ccc0fbae3ce74b71208ba42afc175
Raj <rajexcited@users.noreply.github.com>
2024-11-18 22:27:38 -0600

    updated qualifier to bootstrapped cdk

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -12,7 +12,9 @@ const stack1 = new HelloCdkStack(app, "HelloCdkStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
-  synthesizer: new cdk.CliCredentialsStackSynthesizer(),
+  synthesizer: new cdk.CliCredentialsStackSynthesizer({
+    qualifier: `${appId}${infraEnv}`,
+  }),
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
