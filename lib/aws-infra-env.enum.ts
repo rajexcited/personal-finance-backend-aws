53ccfcfa396b279313d20947c3db3af4e2fbc93f
Raj <rajexcited@users.noreply.github.com>
2024-11-28 10:06:13 -0600

    added dev env

--- lib/aws-infra-env.enum.ts
+++ lib/aws-infra-env.enum.ts
@@ -1,11 +1,12 @@
 export enum AwsInfraEnvironment {
   TEST = "tst",
+  Development = "dev",
   UNKNOWN = "unknown",
 }
 
 export const getValidAwsInfraEnvironment = () => {
   const infraEnv = <AwsInfraEnvironment>process.env.INFRA_ENV;
-  if ([AwsInfraEnvironment.TEST, AwsInfraEnvironment.UNKNOWN].includes(infraEnv)) {
+  if ([AwsInfraEnvironment.TEST, AwsInfraEnvironment.UNKNOWN, AwsInfraEnvironment.Development].includes(infraEnv)) {
     return infraEnv;
   }
   throw new Error("valid infra env not provided");
