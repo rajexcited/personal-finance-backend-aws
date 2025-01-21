ad9026efa6db0ee24bd440d5016f0b0c43fcf931
Raj <rajexcited@users.noreply.github.com>
2025-01-20 19:13:48 -0600

    workflow update to support new end2end flow - build job when branch is created - updated environment references. for master, prod otherwise test. - added support to deploy ui and infra for test when requested

--- src/infra-lib/common/env-enum.ts
+++ src/infra-lib/common/env-enum.ts
@@ -1,12 +1,13 @@
 export enum InfraEnvironmentId {
   Development = "dev",
+  Test = "tst",
   Production = "prd",
-  UNKNOWN = "undefined",
+  UNKNOWN = "undefined"
 }
 
 export const getValidInfraEnvironment = () => {
   const infraEnv = <InfraEnvironmentId>process.env.INFRA_ENV;
-  if ([InfraEnvironmentId.UNKNOWN, InfraEnvironmentId.Development, InfraEnvironmentId.Production].includes(infraEnv)) {
+  if ([InfraEnvironmentId.UNKNOWN, InfraEnvironmentId.Development, InfraEnvironmentId.Test, InfraEnvironmentId.Production].includes(infraEnv)) {
     return infraEnv;
   }
   throw new Error("valid infra env not provided, found this [" + infraEnv + "]");
