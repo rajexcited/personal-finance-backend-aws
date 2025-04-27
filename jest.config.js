c5dc2a200427347d6b3a71cabdfa8aa2b08160b0
Raj <rajexcited@users.noreply.github.com>
2025-04-27 16:16:26 -0500

    updated ecma version to 2022; added eslint and updated package files

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testMatch: ["**/*.test.ts"],
   transform: { "^.+\\.tsx?$": "ts-jest" },
   collectCoverage: true, // Ensure coverage collection is enabled
+  coverageDirectory: "dist/reports/coverage",
   coveragePathIgnorePatterns: [
     "/node_modules/",
     "/test/test-utils/" // Ignore utility directory
