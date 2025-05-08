ec8b47c0db7826eb0eea29db1f8923c1a720777f
Raj <rajexcited@users.noreply.github.com>
2025-05-08 06:59:05 -0500

    merged 2 s3 deploy

--- src/infra-lib/cf-distribution/ui-asset-deploy-s3.ts
+++ src/infra-lib/cf-distribution/ui-asset-deploy-s3.ts
@@ -1,6 +1,7 @@
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as logs from "aws-cdk-lib/aws-logs";
 import { AwsResourceType, buildResourceName, ConstructProps } from "../common";
 
 interface UiAssetDeployS3Props extends ConstructProps {
@@ -15,30 +16,11 @@ export class UiAssetDeployS3Construct extends Construct {
 
     const uiBucket = s3.Bucket.fromBucketArn(this, "UiBucketFromArn", props.uiBucketArn);
 
-    const uiStaticBucketDeploymentConstructId = buildResourceName(["ui", "static"], AwsResourceType.BucketDeployment, props);
+    const uiStaticBucketDeploymentConstructId = buildResourceName(["ui", "static", "site"], AwsResourceType.BucketDeployment, props);
     const uiDeployment = new s3Deploy.BucketDeployment(this, uiStaticBucketDeploymentConstructId, {
       destinationBucket: uiBucket,
-      sources: [
-        s3Deploy.Source.asset("dist/ui", {
-          exclude: ["**/*.html"]
-        })
-      ],
-      destinationKeyPrefix: props.uiPathPrefix.slice(1) + "/"
+      sources: [s3Deploy.Source.asset("dist/ui")],
+      logRetention: logs.RetentionDays.ONE_MONTH
     });
-
-    const homepageBucketDeploymentConstructId = buildResourceName(["homepage"], AwsResourceType.BucketDeployment, props);
-    const homepageDeployment = new s3Deploy.BucketDeployment(this, homepageBucketDeploymentConstructId, {
-      destinationBucket: uiBucket,
-      sources: [
-        s3Deploy.Source.asset("dist/ui", {
-          exclude: ["**/static/**", "*.jpeg", "*.png", "*.json", "*.ico", "*.txt", "*.css", "*.js"]
-        })
-      ],
-      destinationKeyPrefix: props.homepagePath + "/"
-    });
-
-    // due to different folder structure of dist against CF S3 structure requirements, if both runs are overlapping, 1 of them will fail.
-    // by having dependency making sure that both runs are in proper order so that we dont loose files and deployment doesn't hang.
-    uiDeployment.node.addDependency(homepageDeployment);
   }
 }
